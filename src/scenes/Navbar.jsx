a03001249a65ad500c4b7fd8c4139ff8ae5c7fd5
Nitish <rdpara1214@gmail.com>
2022-12-03 20:42:02 +0530

    sidenav transition added

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -9,19 +9,20 @@ function Navbar() {
   const [isMenuToggle, setIsMenuToggle] = useState(false);
   const isMediumScreen = UseMediaQuery("(min-width:768px)");
   const [isScreenTop, setIsScreenTop] = useState(true);
-  const [navbarBgColor, setnavbarBgColor] = useState("");
+
+  const listenScroll = () => {
+    if (window.scrollY !== 0) {
+      setIsScreenTop(false);
+    }
+    if (window.scrollY === 0) {
+      setIsScreenTop(true);
+    }
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY !== 0) {
-        setnavbarBgColor("#273444");
-      }
-      if (window.scrollY === 0) {
-        setnavbarBgColor("");
-      }
-    });
+    window.addEventListener("scroll", listenScroll);
 
-    return () => {};
+    return () => window.removeEventListener("scroll", listenScroll);
   }, [isScreenTop]);
 
   const handleMenu = () => {
@@ -31,8 +32,9 @@ function Navbar() {
   return (
     <>
       <div
-        className="border-b-white border-b-2 flex justify-between items-center sticky top-0 h-28 w-full z-10 font-heebo"
-        style={{ backgroundColor: navbarBgColor }}
+        className={`border-b-white border-b-2 flex justify-between items-center sticky top-0 h-28 w-full z-10 font-heebo ${
+          window.scrollY !== 0 ? "bg-dark-grey" : "bg-none"
+        }`}
       >
         {/* Brand Logo  */}
         <div className="brand-logo font-extrabold p-8 text-3xl ">
@@ -58,34 +60,37 @@ function Navbar() {
           </div>
         )}
       </div>
-      {isMenuToggle && !isMediumScreen && (
-        <div className="fixed z-20">
-          <div
-            className="backdrop w-full h-full bg-light-gray fixed z-1"
-            onClick={handleMenu}
-          ></div>
-          <div className="h-full w-2/3 fixed z-20 bg-blue right-0 flex flex-col">
-            <div className="ml-auto p-4 mb-4" onClick={handleMenu}>
-              <img
-                src={closeMenu}
-                alt="close-icon"
-                className="h-[40px] w-[40px]"
-              />
-            </div>
-            <ul
-              className="flex flex-col items-center gap-5 px-4 mx-4 font-semibold"
-              onClick={handleMenu}
-            >
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#contact"> Contact Us</a>
-              </li>
-            </ul>
+      {/* Sidenav for small screen */}
+      <div
+        className={`fixed z-20 transition-transform ease-linear duration-1000 h-full w-full translate-x-full ${
+          isMenuToggle ? "translate-x-0 " : "translate-x-full"
+        }`}
+      >
+        <div
+          className="backdrop w-full h-full bg-light-gray fixed z-1"
+          onClick={handleMenu}
+        ></div>
+        <div className="h-full w-2/3 fixed z-20 bg-blue right-0 flex flex-col">
+          <div className="ml-auto p-4 mb-4" onClick={handleMenu}>
+            <img
+              src={closeMenu}
+              alt="close-icon"
+              className="h-[40px] w-[40px]"
+            />
           </div>
+          <ul
+            className="flex flex-col items-center gap-5 px-4 mx-4 font-semibold"
+            onClick={handleMenu}
+          >
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact"> Contact Us</a>
+            </li>
+          </ul>
         </div>
-      )}
+      </div>
     </>
   );
 }
